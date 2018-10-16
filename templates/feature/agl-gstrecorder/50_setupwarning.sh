102af40f38dc838a6df5c0b9c66f5d4fb2614a41
Jan-Simon Möller <jsmoeller@linuxfoundation.org>
2018-10-16 18:20:27 +0200

    Add necessary DISTRO_FEATURES to gstrecorder template
    These are missing to enable the whole machinery on the sender side.
    
    CAVEAT: The network between sender and receiver needs to be up
            (192.168.20.x/24) or weston will not start!
    
    Change-Id: Ib478f0840dd3e6d28f9ca14540302c872c5bcd7b
    Signed-off-by: Jan-Simon Möller <jsmoeller@linuxfoundation.org>

---
+++ templates/feature/agl-gstrecorder/50_setupwarning.sh
@@ -0,0 +1,14 @@
+#Note to the user!
+
+echo "##################################"
+echo "# ATTENTION: the network between #"
+echo "#  sender and receiver of the    #"
+echo "#  gstrecorder feed needs to be  #"
+echo "#  configured or                 #"
+echo "#  WESTON WILL FAIL TO START !   #"
+echo "#                                #"
+echo "#  (hint: 192.168.20.x)          #"
+echo "##################################"
+
+sleep 2
+
